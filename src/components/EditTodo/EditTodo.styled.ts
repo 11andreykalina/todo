b6021bee8636a5e9b35918d27deb72b1a1648c09
11andreykalina <11andreykalina@gmail.com>
2026-01-22 10:28:28 +0300

    v.11

--- src/components/EditTodo/EditTodo.styled.ts
+++ src/components/EditTodo/EditTodo.styled.ts
@@ -2,25 +2,30 @@ import styled from 'styled-components';
 
 
 export const StyledInput = styled.input`  
-  max-width: 110px;
+  max-width: 90px;
   max-height: 36px;
 `;
 
 export const SaveButton = styled.button`
   margin-left: 8px;
-  max-width: 98px;
-  max-height: 36px;
+  width: 36px;
+  height: 36px;
   word-wrap: break-word;
   overflow-wrap: break-word;
   font-size: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;  
 export const CancelButton = styled.button`
-  max-width: 98px;
+  max-width: 36px;
   max-height: 36px;
   word-wrap: break-word;
   overflow-wrap: break-word;
   font-size: 12px;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;  
 
 export const Styled = {
