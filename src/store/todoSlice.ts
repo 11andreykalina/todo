bf3ba68fccf6f68ecc635ede018bf9faacfa2330
11andreykalina <11andreykalina@gmail.com>
2026-02-10 11:58:24 +0300

    render-serv

--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -2,11 +2,8 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import type { Todo } from "../types";
 import { FilterTypeEnum } from "../constants";
-import { mockTodos } from "./mockTodos";
 
-const API_URL = import.meta.env.DEV
-  ? "http://localhost:3001"
-  : null;
+const API_URL = "https://todo-api-server-ixbr.onrender.com";
 
 interface TodoState {
   todos: Todo[];
@@ -36,14 +33,6 @@ export const loadTodos = createAsyncThunk(
     page: number;
     filter: FilterTypeEnum;
   }) => {
-    if (!API_URL) {
-      return {
-        data: mockTodos,
-        page: 1,
-        totalPages: 1,
-      };
-    }
-
     const response = await axios.get(`${API_URL}/todos`, {
       params: {
         ...params,
