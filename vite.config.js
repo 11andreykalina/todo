954930faf8928a63b2d45ab26340140fc3ce5746
11andreykalina <11andreykalina@gmail.com>
2026-02-26 21:36:51 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
+    base: "/todo-app/",
     plugins: [react()],
-    base: "/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
